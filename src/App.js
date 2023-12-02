dfdb428f2d149557728dc1f9356bbcedab69c18d
Shyam <89705591+shyamguptaa@users.noreply.github.com>
2023-12-02 21:21:18 +0530

    nav_changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
+// import '../node_modules/font-awesome/css/font-awesome.min.css';
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
   return (
+   <>
+    <Navbar />
     <div className="App">
-     
+     BODY
     </div>
+    <Footer/>
+   </>
   );
 }
 
